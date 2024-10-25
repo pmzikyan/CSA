ba3b4c8d0bb03f8f9a4a2e674e80db619f54eaef
Petros Mzikyan <103164712+pmzikyan@users.noreply.github.com>
2024-10-25 15:03:08 -0700

    Add files via upload

--- Files/Yahtzee/Yahtzee.java
+++ Files/Yahtzee/Yahtzee.java
@@ -6,15 +6,24 @@
 */
 public class Yahtzee {	
 	
-	YahtzeePlayer player1, player2;
-	Prompt ask;
-	DiceGroup diceGroup;
+	private final int NUM_ROUNDS = 13;
+	private final int NUM_PLAYERS = 2;
+	
+	private YahtzeePlayer player1, player2;
+	private Prompt ask;
+	private DiceGroup diceGroup;
+	
+	private boolean player1Turn;
+	private int round;
 	
 	public Yahtzee() {
 		player1 = new YahtzeePlayer();
 		player2 = new YahtzeePlayer();
 		ask = new Prompt();
 		diceGroup = new DiceGroup();
+		
+		player1Turn = true;
+		round = 0;
 	}
 	
 	public static void main(String[] args) {
@@ -24,14 +33,64 @@ public class Yahtzee {
 		yahtzee.run();
 	}
 	
+	public void run() {
+		while (round < 13)
+		{
+			round++;
+			do {
+				printScore();
+				YahtzeePlayer currentPlayer;
+				if (player1Turn) {
+					currentPlayer = player1;
+					System.out.println("\nRound " + round + " of " + 
+									NUM_ROUNDS + " rounds.\n");
+				}
+				else
+					currentPlayer = player2;
+				
+				ask.getString("\n" + currentPlayer.getName() + 
+		", it's your turn to play. Please hit enter to roll the dice");
+		
+				diceGroup.rollDice();
+				diceGroup.printDice();
+				
+				int diceToChange = ask.getString("Which di(c)e would you like to keep?"
++ "Enter the values you'd like to 'hold' without spaces. For examples, " 
++ "if you'd like to 'hold' die 1, 2, and 5, enter 125 (enter -1 if you'd like to end the turn)");
+				
+				player1Turn = !player1Turn;
+			}
+			while (player1Turn);
+		}
+	}
+	
+	private void printScore() {
+		player1.getScoreCard().printCardHeader();
+		player1.getScoreCard().printPlayerScore(player1);
+		player2.getScoreCard().printPlayerScore(player2);
+	}
+	
+	/**
+	 *  Does the pre-game preperation (getting the players' names and
+	 * 	getting who goes first and who goes second)
+	 */
 	public void preGame() {
-		String name1 = ask.getString("Player 1, please enter your first name");
-		String name2 = ask.getString("\nPlayer 2, please enter your first name");
+		String name1, name2;
+		name1 = name2 = "";
+		
+		do
+			name1 = ask.getString("Player 1, please enter your first name");
+		while (name1.length() < 1 || name1.length() > 12);
+		
+		System.out.println();
+		do
+			name2 = ask.getString("Player 2, please enter your first name");
+		while (name2.length() < 1 || name2.length() > 12 || name2.equals(name1));
 		
 		int temp1, temp2;
 		temp1 = temp2 = 0;
 		
-		while (temp1 != temp2)
+		while (temp1 == temp2)
 		{
 			ask.getString("\nLet's see who will go first. " + name1 + 
 								", please hit enter to roll the dice ");
@@ -50,11 +109,20 @@ public class Yahtzee {
 					
 			if (temp1 == temp2)
 				ask.getString("\nThe sums were a tie! Press enter to roll again");
-		}	
-	}
-	
-	public void run() {
+		}
 		
+		if (temp1 > temp2) {
+			System.out.println(name1 + 
+					", since your sum was higher, you'll roll first.");
+			player1.setName(name1);
+			player2.setName(name2);
+		}
+		else {
+			System.out.println(name2 + 
+					", since your sum was higher, you'll roll first.");
+			player1.setName(name2);
+			player2.setName(name1);
+		}
 	}
 	
 	public void printHeader() {
