e7c6eb52bcb331d9cb88a36f1660d0ec1c9e7305
Petros Mzikyan <103164712+pmzikyan@users.noreply.github.com>
2025-01-10 14:36:42 -0800

    Add files via upload

---
+++ Files/WordFinder/SortMethods.java
@@ -0,0 +1,112 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;		// for testing purposes
+
+/**
+ *	SortMethods - Sorts an ArrayList of Strings in ascending order.
+ *
+ *	Requires FileUtils class to compile.
+ *	Requires file randomWords.txt to execute a test run.
+ *
+ *	@author	Petros Mzikyan
+ *	@since	1/10/2025
+ */
+public class SortMethods {
+	
+	/**
+	 *	Merge Sort algorithm - in ascending order
+	 *	@param arr		List of String objects to sort
+	 */
+	public void mergeSort(List<String> arr) {
+		mergeSortRecurse(arr, 0, arr.size() - 1);
+	}
+	
+	/**
+	 *	Recursive mergeSort method.
+	 *	@param arr		List of String objects to sort
+	 *	@param first	first index of arr to sort
+	 *	@param last		last index of arr to sort
+	 */
+	public void mergeSortRecurse(List<String> arr, int first, int last) {
+		// insert your code here
+		int size = last - first + 1;
+		//System.out.println(size);
+		int mid = first + size/2;
+		//System.out.println(mid + ", " + size);
+		
+		if (size > 2) {
+			mergeSortRecurse(arr, first, mid);
+			mergeSortRecurse(arr, mid + 1, last);
+		}
+		merge(arr, first, mid, last);
+		
+	}
+	
+	/**
+	 *	Merge two lists that are consecutive elements in array.
+	 *	@param arr		List of String objects to merge
+	 *	@param first	first index of first list
+	 *	@param mid		the last index of the first list;
+	 *					mid + 1 is first index of second list
+	 *	@param last		last index of second list
+	 */
+	public void merge(List<String> arr, int first, int mid, int last) {
+		// Insert your code here
+		int index1 = first, index2 = mid + 1;
+		while (index1 < index2 && index2 < last) {
+			if (arr.get(index1).compareTo(arr.get(index2)) > 0)
+				arr.add(index1, arr.remove(index2));
+			
+			index1++;
+			index2++;
+		}
+	}
+
+	
+	/**
+	 *	Print an List of Strings to the screen
+	 *	@param arr		the List of Strings
+	 */
+	public void printArray(List<String> arr) {
+		if (arr.size() == 0) System.out.print("(");
+		else System.out.printf("( %-15s", arr.get(0));
+		for (int a = 1; a < arr.size(); a++) {
+			if (a % 5 == 0) System.out.printf(",\n  %-15s", arr.get(a));
+			else System.out.printf(", %-15s", arr.get(a));
+		}
+		System.out.println(" )");
+	}
+	
+	/*************************************************************/
+	/********************** Test program *************************/
+	/*************************************************************/
+	private final String FILE_NAME = "randomWords.txt";
+	
+	public static void main(String[] args) {
+		SortMethods se = new SortMethods();
+		se.run();
+	}
+	
+	public void run() {
+		List<String> arr = new ArrayList<String>();
+		// Fill List with random words from file		
+		fillArray(arr);
+		
+		System.out.println("\nMerge Sort");
+		System.out.println("Array before sort:");
+		printArray(arr);
+		System.out.println();
+		mergeSort(arr);
+		System.out.println("Array after sort:");
+		printArray(arr);
+		System.out.println();
+	}
+	
+	// Fill String array with words
+	public void fillArray(List<String> arr) {
+		Scanner inFile = FileUtils.openToRead(FILE_NAME);
+		while (inFile.hasNext())
+			arr.add(inFile.next());
+		inFile.close();
+	}
+}
