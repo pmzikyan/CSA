6ec1af1bdc8484a4e3586984d5e2c6c2b7baa619
Petros Mzikyan <103164712+pmzikyan@users.noreply.github.com>
2025-03-26 12:57:41 -0700

    idk

--- Files/GridWorldCode/projects/critters/ChameleonCritter.java
+++ Files/GridWorldCode/projects/critters/ChameleonCritter.java
@@ -29,6 +29,8 @@ import java.util.ArrayList;
  */
 public class ChameleonCritter extends Critter
 {
+    private static final double DARKENING_FACTOR = 0.05;
+
     /**
      * Randomly selects a neighbor and changes this critter's color to be the
      * same as that neighbor's. If there are no neighbors, no action is taken.
@@ -37,11 +39,26 @@ public class ChameleonCritter extends Critter
     {
         int n = actors.size();
         if (n == 0)
+        {
+            darken();
             return;
-        int r = (int) (Math.random() * n);
+        }
 
+        int r = (int) (Math.random() * n);
         Actor other = actors.get(r);
         setColor(other.getColor());
+    }
+
+    /**
+     * Darkens this critter's color by DARKENING_FACTOR.
+     */
+    private void darken()
+    {
+        Color c = getColor();
+        int red = (int) (c.getRed() * (1 - DARKENING_FACTOR));
+        int green = (int) (c.getGreen() * (1 - DARKENING_FACTOR));
+        int blue = (int) (c.getBlue() * (1 - DARKENING_FACTOR));
+        setColor(new Color(red, green, blue));
     }
 
     /**
---
+++ Files/GridWorldCode/projects/critters/ChameleonKid.java
@@ -0,0 +1,73 @@
+/* 
+ * AP(r) Computer Science GridWorld Case Study:
+ * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author Chris Nevison
+ * @author Barbara Cloud Wells
+ * @author Cay Horstmann
+ */
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.actor.Flower;
+import info.gridworld.grid.Location;
+import info.gridworld.grid.Grid;
+import java.util.ArrayList;
+/**
+ * A <code>ChameleonKid</code> takes on the color of neighboring actors
+ * that are in front or behind as it moves through the grid. <br />
+ */
+public class ChameleonKid extends ChameleonCritter
+{
+    /**
+     * Gets the actors for processing. The actors must be contained in the
+     * same grid as this critter. Implemented to return the actors that
+     * occupy neighboring grid locations in front or behind this critter.
+     * @return a list of actors that are neighbors of this critter
+     */
+    public ArrayList<Actor> getActors()
+    {
+        ArrayList<Actor> actors = new ArrayList<Actor>();
+        int[] dirs =
+                { Location.AHEAD, Location.HALF_CIRCLE };
+        for (Location loc : getLocationsInDirections(dirs))
+        {
+            Actor a = getGrid().get(loc);
+            if (a != null)
+                actors.add(a);
+        }
+        return actors;
+    }
+
+    /**
+     * Finds the valid adjacent locations of this critter in different
+     * directions.
+     * @param directions - an array of directions (which are relative to the
+     * current direction)
+     * @return a set of valid locations that are neighbors of the current
+     * location in the given directions
+     */
+    public ArrayList<Location> getLocationsInDirections(int[] directions)
+    {
+        ArrayList<Location> locs = new ArrayList<Location>();
+        Grid gr = getGrid();
+        Location loc = getLocation();
+
+        for (int d : directions)
+        {
+            Location neighborLoc = loc.getAdjacentLocation(getDirection() + d);
+            if (gr.isValid(neighborLoc))
+                locs.add(neighborLoc);
+        }
+        return locs;
+    }
+}
