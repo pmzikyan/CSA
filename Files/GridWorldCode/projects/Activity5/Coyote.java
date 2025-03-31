ea64db24c9553192a95b4d65c675d49a33865603
Petros Mzikyan <103164712+pmzikyan@users.noreply.github.com>
2025-03-31 14:59:54 -0700

    Add files via upload

--- Files/GridWorldCode/projects/Activity5/Coyote.java
+++ Files/GridWorldCode/projects/Activity5/Coyote.java
@@ -14,21 +14,67 @@ public class Coyote extends Critter
 {
 	private final int THRESHOLD = 5;
 	private int steps;
-	private boolean sleeping;
+	private boolean sleeping, placeStone;
 	
 	public Coyote() 
 	{ 
 		steps = 0;
 		sleeping = false;
+		placeStone = false;
 		setDirection((int)(Math.random()*8)*45);
 		setColor(null); 
 	}
 	
 	public ArrayList<Actor> getActors()
 	{
-		
-		getLocation().getAdjacentLocation(getDirection());
+		ArrayList<Actor> out = new ArrayList<Actor>();
+		Location loc = getLocation().getAdjacentLocation(getDirection());
+		if (getGrid().isValid(loc)) out.add(getGrid().get(loc));
+		return out;
 	}
 	
+	
 	public void processActors(ArrayList<Actor> actors) { }
+	
+	public ArrayList<Location> getMoveLocations()
+	{
+		ArrayList<Location> out = new ArrayList<Location>();
+		Location loc = getLocation().getAdjacentLocation(getDirection());
+		
+		if (!getGrid().isValid(loc));
+		else if (getGrid().get(loc) == null) out.add(loc);
+		else
+		{
+			sleeping = !sleeping;
+			steps = 0;
+		}
+		
+		return out;
+	}
+	
+	public void makeMove(Location loc)
+    {
+        if (loc == null)
+            removeSelfFromGrid();
+        else if (!sleeping)
+        {
+            Location oldLoc = getLocation();
+            moveTo(loc);
+            if (placeStone)
+            {
+				Stone stone = new Stone();
+				stone.putSelfInGrid(getGrid(), oldLoc);
+				placeStone = false;
+			}
+		}
+        
+        steps++;
+        if (steps == THRESHOLD)
+        {
+			sleeping = !sleeping;
+			steps = 0;
+			if (!sleeping) placeStone = true;
+			else setDirection((int)(Math.random()*8)*45);
+		}
+    }
 }
